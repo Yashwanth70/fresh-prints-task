9cb3de65c70a8757d8da48cf22e47618fcb89e4e
Yashwanth70 <yash.mydam@gmail.com>
2018-02-03 16:00:35 +0530

    Image upload setup

--- app.js
+++ app.js
@@ -34,9 +34,12 @@ connection.connect(function(err, results) {
             });
 
 });
+//Image Upload setup
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+// var index = require('./routes/index');
+// var users = require('./routes/users');
 
 var app = express();
 
@@ -52,8 +55,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+// app.use('/', index);
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
